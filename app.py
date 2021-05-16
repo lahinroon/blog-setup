55fcde28a401a0e73bb3ca574337380f703b0ed7
Nihal <nihalnoor007@gmail.com>
2021-05-16 20:31:48 +1000

    Working skeleton of blog app.py changed

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-# app.py
 import datetime
 import functools
 import os
 import re
 import urllib
 
-from flask import (Flask, abort, flash, Markup, redirect, render_template,
-                   request, Response, session, url_for)
+from flask import (Flask, flash, Markup, redirect, render_template, request,
+                   Response, session, url_for)
 from markdown import markdown
 from markdown.extensions.codehilite import CodeHiliteExtension
 from markdown.extensions.extra import ExtraExtension
@@ -16,24 +15,45 @@ from peewee import *
 from playhouse.flask_utils import FlaskDB, get_object_or_404, object_list
 from playhouse.sqlite_ext import *
 
+
+# Blog configuration values.
+
+# You may consider using a one-way hash to generate the password, and then
+# use the hash again in the login view to perform the comparison. This is just
+# for simplicity.
 ADMIN_PASSWORD = 'secret'
 APP_DIR = os.path.dirname(os.path.realpath(__file__))
+
+# The playhouse.flask_utils.FlaskDB object accepts database URL configuration.
 DATABASE = 'sqliteext:///%s' % os.path.join(APP_DIR, 'blog.db')
 DEBUG = False
-SECRET_KEY = 'shhh, secret!'  # Used by Flask to encrypt session cookie.
+
+# The secret key is used internally by Flask to encrypt session data stored
+# in cookies. Make this unique for your app.
+SECRET_KEY = 'shhh, secret!'
+
+# This is used by micawber, which will attempt to generate rich media
+# embedded objects with maxwidth=800.
 SITE_WIDTH = 800
 
 
+# Create a Flask WSGI app and configure it using values from the module.
 app = Flask(__name__)
 app.config.from_object(__name__)
 
+# FlaskDB is a wrapper for a peewee database that sets up pre/post-request
+# hooks for managing database connections.
 flask_db = FlaskDB(app)
+
+# The `database` is the actual peewee database, as opposed to flask_db which is
+# the wrapper.
 database = flask_db.database
 
+# Configure micawber with the default OEmbed providers (YouTube, Flickr, etc).
+# We'll use a simple in-memory cache so that multiple requests for the same
+# video don't require multiple network requests.
 oembed_providers = bootstrap_basic(OEmbedCache())
 
-
-'''Database Schema - Entry Class'''
 
 class Entry(flask_db.Model):
     title = CharField()
@@ -42,55 +62,13 @@ class Entry(flask_db.Model):
     published = BooleanField(index=True)
     timestamp = DateTimeField(default=datetime.datetime.now, index=True)
 
-    '''Create URL for page entry'''
-    @classmethod
-    def save(self, *args, **kwargs):
-        if not self.slug:
-            self.slug = re.sub('[^\w]+', '-', self.title.lower())
-        ret = super(Entry, self).save(*args, **kwargs)
-
-        # Store search content.
-        self.update_search_index()
-        return ret
-
-    @classmethod
-    def update_search_index(self):
-        search_content = '\n'.join((self.title, self.content))
-        try:
-            fts_entry = FTSEntry.get(FTSEntry.docid == self.id)
-        except FTSEntry.DoesNotExist:
-            FTSEntry.create(docid=self.id, content=search_content)
-        else:
-            fts_entry.content = search_content
-            fts_entry.save()
-
-    @classmethod
-    def public(cls):
-        return Entry.select().where(Entry.published == True)
-
-    @classmethod
-    def search(cls, query):
-        words = [word.strip() for word in query.split() if word.strip()]
-        if not words:
-            # Return empty query.
-            return Entry.select().where(Entry.id == 0)
-        else:
-            search = ' '.join(words)
-
-        return (Entry
-                .select(Entry, FTSEntry.rank().alias('score'))
-                .join(FTSEntry, on=(Entry.id == FTSEntry.docid))
-                .where(
-                    (Entry.published == True) &
-                    (FTSEntry.match(search)))
-                .order_by(SQL('score')))
-
-    @classmethod
-    def drafts(cls):
-        return Entry.select().where(Entry.published == False)
-
     @property
     def html_content(self):
+        """
+        Generate HTML representation of the markdown-formatted blog entry,
+        and also convert any media URLs into rich media objects such as video
+        players or images.
+        """
         hilite = CodeHiliteExtension(linenums=False, css_class='highlight')
         extras = ExtraExtension()
         markdown_content = markdown(self.content, extensions=[hilite, extras])
@@ -101,37 +79,69 @@ class Entry(flask_db.Model):
             maxwidth=app.config['SITE_WIDTH'])
         return Markup(oembed_content)
 
+    def save(self, *args, **kwargs):
+        # Generate a URL-friendly representation of the entry's title.
+        if not self.slug:
+            self.slug = re.sub(r'[^\w]+', '-', self.title.lower()).strip('-')
+        ret = super(Entry, self).save(*args, **kwargs)
+
+        # Store search content.
+        self.update_search_index()
+        return ret
+
+    def update_search_index(self):
+        # Create a row in the FTSEntry table with the post content. This will
+        # allow us to use SQLite's awesome full-text search extension to
+        # search our entries.
+        exists = (FTSEntry
+                  .select(FTSEntry.docid)
+                  .where(FTSEntry.docid == self.id)
+                  .exists())
+        content = '\n'.join((self.title, self.content))
+        if exists:
+            (FTSEntry
+             .update({FTSEntry.content: content})
+             .where(FTSEntry.docid == self.id)
+             .execute())
+        else:
+            FTSEntry.insert({
+                FTSEntry.docid: self.id,
+                FTSEntry.content: content}).execute()
+
+    @classmethod
+    def public(cls):
+        return Entry.select().where(Entry.published == True)
+
+    @classmethod
+    def drafts(cls):
+        return Entry.select().where(Entry.published == False)
+
+    @classmethod
+    def search(cls, query):
+        words = [word.strip() for word in query.split() if word.strip()]
+        if not words:
+            # Return an empty query.
+            return Entry.noop()
+        else:
+            search = ' '.join(words)
+
+        # Query the full-text search index for entries matching the given
+        # search query, then join the actual Entry data on the matching
+        # search result.
+        return (Entry
+                .select(Entry, FTSEntry.rank().alias('score'))
+                .join(FTSEntry, on=(Entry.id == FTSEntry.docid))
+                .where(
+                    FTSEntry.match(search) &
+                    (Entry.published == True))
+                .order_by(SQL('score')))
 
 class FTSEntry(FTSModel):
-    content = SearchField()
+    content = TextField()
 
     class Meta:
         database = database
 
-#### App initialization code for Flask ####
-
-@app.template_filter('clean_querystring')
-def clean_querystring(request_args, *keys_to_remove, **new_values):
-    querystring = dict((key, value) for key, value in request_args.items())
-    for key in keys_to_remove:
-        querystring.pop(key, None)
-    querystring.update(new_values)
-    return urllib.urlencode(querystring)
-
-@app.errorhandler(404)
-def not_found(exc):
-    return Response('<h3>Not found</h3>')
-
-def main():
-    database.create_tables([Entry, FTSEntry])
-    app.run(debug=True)
-
-if __name__ == '__main__':
-    main()
-
-#### Login Verification ####
-
-#Redirect to login page if not logged in, else redirect to requested page
 def login_required(fn):
     @functools.wraps(fn)
     def inner(*args, **kwargs):
@@ -140,13 +150,13 @@ def login_required(fn):
         return redirect(url_for('login', next=request.path))
     return inner
 
-'''__________________ FLASK VIEWPAGES ______________ '''
-
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     next_url = request.args.get('next') or request.form.get('next')
     if request.method == 'POST' and request.form.get('password'):
         password = request.form.get('password')
+        # TODO: If using a one-way hash, you would also hash the user-submitted
+        # password and do the comparison on the hashed versions.
         if password == app.config['ADMIN_PASSWORD']:
             session['logged_in'] = True
             session.permanent = True  # Use cookie to store session.
@@ -163,7 +173,6 @@ def logout():
         return redirect(url_for('login'))
     return render_template('logout.html')
 
-#### Flask index page ####
 @app.route('/')
 def index():
     search_query = request.args.get('q')
@@ -171,34 +180,52 @@ def index():
         query = Entry.search(search_query)
     else:
         query = Entry.public().order_by(Entry.timestamp.desc())
-    return object_list('index.html', query, search=search_query)
 
-##### View for unpublished posts ####
-@app.route('/drafts/')
-@login_required
-def drafts():
-    query = Entry.drafts().order_by(Entry.timestamp.desc())
-    return object_list('index.html', query)
+    # The `object_list` helper will take a base query and then handle
+    # paginating the results if there are more than 20. For more info see
+    # the docs:
+    # http://docs.peewee-orm.com/en/latest/peewee/playhouse.html#object_list
+    return object_list(
+        'index.html',
+        query,
+        search=search_query,
+        check_bounds=False)
+
+def _create_or_edit(entry, template):
+    if request.method == 'POST':
+        entry.title = request.form.get('title') or ''
+        entry.content = request.form.get('content') or ''
+        entry.published = request.form.get('published') or False
+        if not (entry.title and entry.content):
+            flash('Title and Content are required.', 'danger')
+        else:
+            # Wrap the call to save in a transaction so we can roll it back
+            # cleanly in the event of an integrity error.
+            try:
+                with database.atomic():
+                    entry.save()
+            except IntegrityError:
+                flash('Error: this title is already in use.', 'danger')
+            else:
+                flash('Entry saved successfully.', 'success')
+                if entry.published:
+                    return redirect(url_for('detail', slug=entry.slug))
+                else:
+                    return redirect(url_for('edit', slug=entry.slug))
+
+    return render_template(template, entry=entry)
 
 @app.route('/create/', methods=['GET', 'POST'])
 @login_required
 def create():
-    if request.method == 'POST':
-        if request.form.get('title') and request.form.get('content'):
-            entry = Entry.create(
-                title=request.form['title'],
-                content=request.form['content'],
-                published=request.form.get('published') or False)
-            flash('Entry created successfully.', 'success')
-            if entry.published:
-                return redirect(url_for('detail', slug=entry.slug))
-            else:
-                return redirect(url_for('edit', slug=entry.slug))
-        else:
-            flash('Title and Content are required.', 'danger')
-    return render_template('create.html')
+    return _create_or_edit(Entry(title='', content=''), 'create.html')
 
-''' Flask page - URL for each entry '''
+@app.route('/drafts/')
+@login_required
+def drafts():
+    query = Entry.drafts().order_by(Entry.timestamp.desc())
+    return object_list('index.html', query, check_bounds=False)
+
 @app.route('/<slug>/')
 def detail(slug):
     if session.get('logged_in'):
@@ -208,25 +235,32 @@ def detail(slug):
     entry = get_object_or_404(query, Entry.slug == slug)
     return render_template('detail.html', entry=entry)
 
-
 @app.route('/<slug>/edit/', methods=['GET', 'POST'])
 @login_required
 def edit(slug):
     entry = get_object_or_404(Entry, Entry.slug == slug)
-    if request.method == 'POST':
-        if request.form.get('title') and request.form.get('content'):
-            entry.title = request.form['title']
-            entry.content = request.form['content']
-            entry.published = request.form.get('published') or False
-            entry.save()
+    return _create_or_edit(entry, 'edit.html')
 
-            flash('Entry saved successfully.', 'success')
-            if entry.published:
-                return redirect(url_for('detail', slug=entry.slug))
-            else:
-                return redirect(url_for('edit', slug=entry.slug))
-        else:
-            flash('Title and Content are required.', 'danger')
+@app.template_filter('clean_querystring')
+def clean_querystring(request_args, *keys_to_remove, **new_values):
+    # We'll use this template filter in the pagination include. This filter
+    # will take the current URL and allow us to preserve the arguments in the
+    # querystring while replacing any that we need to overwrite. For instance
+    # if your URL is /?q=search+query&page=2 and we want to preserve the search
+    # term but make a link to page 3, this filter will allow us to do that.
+    querystring = dict((key, value) for key, value in request_args.items())
+    for key in keys_to_remove:
+        querystring.pop(key, None)
+    querystring.update(new_values)
+    return urllib.urlencode(querystring)
 
-    return render_template('edit.html', entry=entry)
+@app.errorhandler(404)
+def not_found(exc):
+    return Response('<h3>Not found</h3>'), 404
 
+def main():
+    database.create_tables([Entry, FTSEntry], safe=True)
+    app.run(debug=True)
+
+if __name__ == '__main__':
+    main()
